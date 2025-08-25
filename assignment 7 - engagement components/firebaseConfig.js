293d00fb7d72c97fe47dc490eb2af2748ddba220
Varun A. Sridhar <126321540+valleyvarun@users.noreply.github.com>
2025-08-25 06:23:18 -0400

    api unhidden

---
+++ assignment 7 - engagement components/firebaseConfig.js
@@ -0,0 +1,12 @@
+// Local-only Firebase web config. Do NOT commit this file.
+// It is git-ignored by ".gitignore".
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBfUQ380DV8UyX3XAXcLD9YR3Xr1C7LgXI",
+  authDomain: "q-n-a-e84a1.firebaseapp.com",
+  databaseURL: "https://q-n-a-e84a1-default-rtdb.firebaseio.com",
+  projectId: "q-n-a-e84a1",
+  storageBucket: "q-n-a-e84a1.firebasestorage.app",
+  messagingSenderId: "1039224628624",
+  appId: "1:1039224628624:web:5963beec4197eb0ebdff15",
+  measurementId: "G-JL0Y6PHFJE"
+};
